039690fe642aaa9d0f9c4ddefe634f3a73f6e313
Jorgesan99 <45881221+Jorgesan99@users.noreply.github.com>
2019-05-01 14:00:39 +0200

    Parrilla.h actualizada

--- 1.0/Parrilla.h
+++ 1.0/Parrilla.h
@@ -16,14 +16,21 @@ class Parrilla {
 	int naulas;
 	int nTFGs;
 	int nProf;
+	int *bitAulas;
+	short** bitProf;
+	bool exito;
 
-	Presentacion *presentaciones;
+	Presentacion **presentaciones;
 
 public: 
+
+	Parrilla();
+	Parrilla(int convocatoria, int nslots, int naulas, int nTFGs, int nProf);
+	
+	void generarParrilla(TFG *listTFG, Profesor *profesores);
+	void generarPresentacion(TFG *listTFG, int TFG, Profesor *profesores);
+	void generarTribunal(int slot, TFG *listTFG, int TFG, Profesor *profesores, int *jurado);
+	~Parrilla();
+
 };
-
-Parrilla generarParrilla(int slots, int aulas, int nTFGs, TFG *listTFG, Profesor *profesores, int numProf);
-void generarPresentacion(int slots, int aulas, TFG *listTFG, int nTFGs, int TFG, Profesor *profesores, int numProf, Parrilla *parrilla, int *bitAulas, short** bitProf, bool *exito);
-void generarTribunal(int slots, int slot, int aulas, TFG *listTFG, int nTFGs, int TFG, Profesor *profesores, int numProf, Parrilla *parrilla, int *bitAulas, short** bitProf, bool *exito, int *jurado);
-
 
